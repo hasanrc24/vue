1e4619fa8e7ae8bd5411646d36addd9ae5802504
hasanrc24 <hasanrc24@gmail.com>
2023-05-15 18:06:55 +0600

    Initial

---
+++ src/storage/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count = state.count + 1
+    },
+    decrement(state) {
+      state.count = state.count - 1
+    }
+  }
+})
+
+export default store
---
+++ src/views/ContactView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import store from '../storage/index'
+// let count = store.state.count
+
+const incre = () => {
+  store.commit('increment')
+}
+const decre = () => {
+  store.commit('decrement')
+}
+</script>
+
+<template>
+  <div class="contact">
+    <div class="">
+      <h1>This is contact page</h1>
+      <p><span class="name">Name: </span>Md. Hasanuzzaman</p>
+      <p>Count: {{ store.state.count }}</p>
+      <div class="count">
+        <button @click="decre">-</button>
+        <button @click="incre">+</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.name {
+  font-weight: bold;
+}
+.count {
+  display: flex;
+}
+button {
+  cursor: pointer;
+  margin: 0 2px;
+}
+@media (min-width: 1024px) {
+  .contact {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
